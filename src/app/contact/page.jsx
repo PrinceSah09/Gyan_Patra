e47ed421540f9576374797b79381aafcd31efdef
Prince Saha <pbaba7001@gmail.xom>
2024-04-20 11:07:04 +0530

    contact: added contact route

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Contact from "@/components/Contact"; 
+
+export default function Home() {
+    return (
+        <>
+            <Navbar/>
+            <Contact/>
+            <Footer/>
+        </>
+    )
+}
