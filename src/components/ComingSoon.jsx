8b6633a5f8faf91395fb231c309609edce0e69a3
Prince Saha <pbaba7001@gmail.xom>
2024-04-20 10:37:23 +0530

    coming-soon: Desinged coming soon component

---
+++ src/components/ComingSoon.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function ComingSoon() {
+    return (
+        <div className="bg-gradient-to-r from-red-500 to-orange-500 py-10">
+            <div
+                className='
+                    max-w-screen-xl
+                    bg-white/50
+                    rounded-xl
+                    my-auto
+                    flex
+                    justify-center 
+                    items-center 
+                    m-auto 
+                    backdrop-blur-lg 
+                '
+            >
+                <div className="h-[650px] flex justify-center items-center ">
+                    <h className="text-3xl font-bold text-zinc-900">Coming Soon</h>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ComingSoon   
