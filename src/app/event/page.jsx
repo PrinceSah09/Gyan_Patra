2be3dc67de1651f9dfc46d43f4b668fc18137f64
Prince Saha <pbaba7001@gmail.xom>
2024-04-20 11:08:39 +0530

    event: added event route

---
+++ src/app/event/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import ComingSoon from "@/components/ComingSoon";
+
+export default function Home() {
+    return (
+        <>
+            <Navbar />
+            <ComingSoon />
+            <Footer />
+        </>
+    )
+}
