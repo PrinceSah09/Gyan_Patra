bb80e47a06a14f651f81cb58cfc73404f1f6a318
Prince Saha <pbaba7001@gmail.com>
2024-04-22 00:05:00 +0530

    upadated: updated text from lorem to dummy

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -12,8 +12,8 @@ function Vision() {
                         <div className='text-9xl'>
                             <FaPeopleGroup />
                         </div>
-                        <h2 className='text-4xl'>lorem sd</h2>
-                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus facere illo cumque.</h3>
+                        <h2 className='text-4xl'>Dummy sd</h2>
+                        <h3>Dummy ipsum dolor sit amet consectetur adipisicing elit. Repellendus facere illo cumque.</h3>
                     </div>
                     <Separator orientation="vertical" className="border h-[250px] mt-10 lg:block hidden" />
                     <Separator orientation="horizontal" className="border mt-10 md:hidden block" />
@@ -21,8 +21,8 @@ function Vision() {
                         <div className='text-9xl'>
                             <FaPeopleGroup />
                         </div>
-                        <h2 className='text-4xl'>lorem sd</h2>
-                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus facere illo cumque.</h3>
+                        <h2 className='text-4xl'>Dummy sd</h2>
+                        <h3>Dummy ipsum dolor sit amet consectetur adipisicing elit. Repellendus facere illo cumque.</h3>
                     </div>
                     <Separator orientation="vertical" className="border h-[250px] mt-10 lg:block hidden" />
                     <Separator orientation="horizontal" className="border mt-10 md:hidden block" />
@@ -30,8 +30,8 @@ function Vision() {
                         <div className='text-9xl'>
                             <FaPeopleGroup />
                         </div>
-                        <h2 className='text-4xl'>lorem sd</h2>
-                        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus facere illo cumque.</h3>
+                        <h2 className='text-4xl'>Dummy sd</h2>
+                        <h3>Dummy ipsum dolor sit amet consectetur adipisicing elit. Repellendus facere illo cumque.</h3>
                     </div>
                 </div>
 
